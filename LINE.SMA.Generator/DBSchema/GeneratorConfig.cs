1d45efc5bdcfd187933bc2050708824f59ce4b90
wenlong lin <linwenlong@rd.keytop.com.cn>
2020-04-29 17:47:58 +0800

    代码生成

---
+++ LINE.SMA.Generator/DBSchema/GeneratorConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINE.SMA.Generator.DBSchema
+{
+    /// <summary>
+    /// 代码生成配置类
+    /// </summary>
+    public class GeneratorConfig
+    {
+        public string dataBaseName { get; set; }
+
+        public string userName { get; set; }
+
+        public string password { get; set; }
+
+
+    }
+}
---
+++ LINE.SMA.Generator/DBSchema/MySqlSchema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINE.SMA.Generator.DBSchema
+{
+    /// <summary>
+    /// 数据库操作类
+    /// </summary>
+    public class MySqlSchema
+    {
+
+    }
+}
